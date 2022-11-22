7909985e17eab059946d8805459fcef9f8f3367e
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2022-11-22 17:48:19 -0600

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -11,3 +11,15 @@ pub struct Suit {
     #[pyo3(get,set)]
     legs: String
 }
+
+#[pyclass]
+pub struct Head {
+    #[pyo3(get,set)]
+    file: String,
+    #[pyo3(get,set)]
+    node: String,
+    #[pyo3(get,set)]
+    texture: Option<String> // head texture may not need to be changed from the node's default
+}
+
+// TODO: head list commands for each head model file: phase_4/suitA-heads.bam, phase_4/suitB-heads.bam, phase_3.5/suitC-heads.bam
