85736378a294a3ac2b4acb8405594d18f8637a7e
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2022-11-23 14:02:27 -0600

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -23,3 +23,32 @@ pub struct Head {
 }
 
 // TODO: head list commands for each head model file: phase_4/suitA-heads.bam, phase_4/suitB-heads.bam, phase_3.5/suitC-heads.bam
+
+/// Taken directly from adopt_a_doodle.
+#[pyclass]
+pub struct Animation {
+    #[pyo3(get,set)]
+    file: String,
+    #[pyo3(get,set)]
+    anim_loop: bool,
+    #[pyo3(get,set)]
+    loop_from: Option<u64>,
+    #[pyo3(get,set)]
+    loop_to: Option<u64>,
+    #[pyo3(get,set)]
+    loop_restart: Option<u64>,
+    #[pyo3(get,set)]
+    pose: bool, // Takes priority over anim_loop - if this is set to true then the actor will be stuck in a pose.
+    #[pyo3(get,set)]
+    pose_frame: Option<u64> // Necessary parameter if pose is true. Sets the frame the cog will be posed at.
+}
+
+#[pymethods]
+impl Animation {
+
+    /// Creates a new Animation struct.
+    #[new]
+    pub fn new(file:String,anim_loop:bool,loop_from:Option<u64>,loop_to:Option<u64>,loop_restart:Option<u64>,pose:bool,pose_frame:Option<u64>) -> Self {
+        Self {file,anim_loop,loop_from,loop_to,loop_restart,pose,pose_frame}
+    }
+}
