50ec79b2ff8beb2e027d318d279874954b3d8e81
Vhou-Atroph <79807240+Vhou-Atroph@users.noreply.github.com>
2022-11-26 17:40:27 -0600

    Update builder.py

--- src/build_a_cog/builder.py
+++ src/build_a_cog/builder.py
@@ -7,12 +7,29 @@ from direct.showbase.Loader import *
 
 from . import rustycog
 
-def gen_head(head:rustycog.Head):
+def _genhead(head:rustycog.Head):
     """Creates a generic cog head according to standard Cog heads."""
-    mod = loader.loadModel(head.file)
-    mod.find("**/"+head.node)
+    mod = loader.loadModel(head.file).find("**/"+head.node)
     if head.texture:
         mod.setTexture(loader.loadTexture(head.texture),1)
     if head.color:
         mod.setColor(head.color)
     return mod
+
+def build(cog:rustycog.Cog):
+    actor = Actor(cog.suit.model,{"animation":cog.animation.file})
+    actor.find('**/hands').setColor(cog.suit.hands)
+    actor.findAllMatches('**/torso').setTexture(loader.loadTexture(cog.suit.torso),1)
+    actor.findAllMatches('**/arms').setTexture(loader.loadTexture(cog.suit.arms),1)
+    actor.findAllMatches('**/legs').setTexture(loader.loadTexture(cog.suit.legs),1)
+
+    if cog.head:
+        head = _genhead(cog.head)
+        head.reparentTo(actor.find('**/def_head'))
+    
+    actor.play("animation")
+    if cog.animation.anim_loop:
+        actor.loop("animation",restart=cog.animation.loop_restart,fromFrame=cog.animation.loop_from,toFrame=cog.animation.loop_to)
+    if cog.animation.pose:
+        actor.pose("animation",cog.pose_frame)
+    return actor
